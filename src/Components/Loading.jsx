92a01a9a6fabb228fbf89feda63450754d58b7fe
Kelly Ramos <kramos@fcc.org.br>
2024-04-26 14:20:15 -0300

    Componente de loading criado e utilizado, axios instalado e utilizado

---
+++ src/Components/Loading.jsx
@@ -0,0 +1,9 @@
+export default function Loading(){
+    return(
+        <div className="loading_container">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    )
+}
